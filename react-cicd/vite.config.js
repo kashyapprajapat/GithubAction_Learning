2fd096fbe79c7833aed2932352be7ae65068f306
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-05-05 17:24:33 +0530

    Fix:Setup GitHub Pages deployment

--- react-cicd/vite.config.js
+++ react-cicd/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/GithubAction_Learning/',
+  base:'/kashyapprajapat-githubaction_learning/',
   plugins: [react()],
   server:{
     open:true,
