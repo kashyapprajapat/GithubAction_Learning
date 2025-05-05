c19921f59c2825595edb1c0599d7b5057dcd5f33
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-05-05 17:31:31 +0530

    Fix:by mistakely in the base url added username

--- react-cicd/vite.config.js
+++ react-cicd/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/kashyapprajapat-githubaction_learning/',
+  base:'/GithubAction_Learning/',
   plugins: [react()],
   server:{
     open:true,
