dfc6974cbbfda5c04ef1e00b6e4374898b77a204
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-05-05 17:34:46 +0530

    changes in the base url

--- react-cicd/vite.config.js
+++ react-cicd/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/GithubAction_Learning/',
+  base:'/GithubAction_Learning/react-cicd/',
   plugins: [react()],
   server:{
     open:true,
