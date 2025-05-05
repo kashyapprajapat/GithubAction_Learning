b72f5183a79e171445539bbb627bd3abe32d3a66
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-05-05 17:20:42 +0530

    Feat:Deploy Vite+React app uisng Github cicd pipeline

---
+++ react-cicd/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <div className="github-actions-box">
+        <h2>🚀 GitHub Actions: CI/CD Made Easy</h2>
+        <p>
+          GitHub Actions automates your workflows — build, test, and deploy your app directly from GitHub!
+        </p>
+        <p className="pulse">🔁 Auto Deploy with Every Push</p>
+        <a href="https://docs.github.com/actions" target="_blank" className="learn-more">
+          Learn more →
+        </a>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
